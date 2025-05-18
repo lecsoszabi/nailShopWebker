0fcfaac71cb27f77d679079ed7c554248f5bbc49
lecsoszabi <ado.szabi@gmail.com>
2025-05-18 22:21:35 +0200

    updated App

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,12 +1,14 @@
+// src/app/models/product.model.ts
 export interface Product {
-  creatorUid: string;
-  id: string;                 // Firestore dokumentum ID
+  id: string;
   name: string;
   price: number;
-  color?: string;              // Specifikus a "Nail"-hez
-  length?: number;             // Specifikus a "Nail"-hez
-  description?: string;        // Rövid leírás, ami a kártyán megjelenhet
-  detailedDescriptionId?: string; // ID a külön Description modellhez, ha van
+  description?: string;
   imageUrl?: string;
-  stock?: number;
+  color?: string;
+  length?: number | null;
+  stock?: number | null;
+  creatorUid: string;
+  createdAt?: any;
+  updatedAt?: any;
 }
